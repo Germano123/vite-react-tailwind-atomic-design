695cafcc835ecd96355dcd58c2452f610afe897c
Germano <germano-software-dev@hotmail.com>
2025-01-08 20:58:45 -0300

    Update form view template

--- src/components/templates/formview/index.tsx
+++ src/components/templates/formview/index.tsx
@@ -8,8 +8,7 @@ const FormViewTemplate: React.FC<FormViewTemplateProps> = ({ children }) => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-white">
       <div className="relative w-full max-w-md p-8 rounded-[24px] bg-neutral">
-        <h1 className="text-4xl text-primary font-bold mb-6 text-center">Didakta</h1>
-        <h2 className="text-2xl mb-6 text-center">Credenciamento</h2>
+        <h1 className="text-4xl text-primary font-bold mb-6 text-center">Project</h1>
         {children}
       </div>
     </div>
