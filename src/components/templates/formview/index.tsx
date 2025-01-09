d3d9f182ae595dfac655238f8dc4fd5cb2010fce
Germano <germano-software-dev@hotmail.com>
2025-01-09 12:41:31 -0300

    Update border in formview template

--- src/components/templates/formview/index.tsx
+++ src/components/templates/formview/index.tsx
@@ -7,7 +7,7 @@ interface FormViewTemplateProps {
 const FormViewTemplate: React.FC<FormViewTemplateProps> = ({ children }) => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-white">
-      <div className="relative w-full max-w-md p-8 rounded-[24px] bg-neutral">
+      <div className="relative w-full max-w-md p-8 border-2 rounded-[24px] bg-neutral">
         <h1 className="text-4xl text-primary font-bold mb-6 text-center">Project</h1>
         {children}
       </div>
